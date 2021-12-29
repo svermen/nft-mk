4d0cc6b81eda853c39f19ea9e83667b6a66f76f9
Santiago Vermeulen <48023484+svermen@users.noreply.github.com>
2021-12-29 11:48:06 -0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,44 @@
 import '../styles/globals.css'
 import Link from 'next/link'
-import logo from '../public/images/fibbo-bl.png';
-import IconButton from '@material-ui/core/IconButton';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-   
-      <nav className="border-b p-6">
-      <img 
-        className="m-2 w-20 sm:w-40 self-center"                                          
-        src="../images/Fibbo-azul.png"
-        
-      />
-        {/* <p src="../images/Fibbo-azul.png" className="text-4xl font-bold" >Fibbo Marketplace</p> */}
-        <div className="flex mt-4">
-          <Link href="/">
-            <a className="mr-4 text-blue-500">
-              Home
-            </a>
-          </Link>
-          <Link href="/create-item">
-            <a className="mr-6 text-blue-500">
-              Sell NFT
-            </a>
-          </Link>
-          <Link href="/my-assets">
-            <a className="mr-6 text-blue-500">
-              My NFTs
-            </a>
-          </Link>
-          <Link href="/creator-dashboard">
-            <a className="mr-6 text-blue-500">
-              Dashboard
-            </a>
-          </Link>
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+        <div className="container flex flex-wrap items-center justify-between mx-auto">
+          <a href="#" className="flex">
+            <img className="m-2 w-20 sm:w-40 self-center" src="../images/Fibbo-azul.png" />
+            {/* <span className="self-center text-lg font-semibold whitespace-nowrap">Fibbo Marketplace</span> */}
+          </a>
+          <input 
+          placeholder="Buscar en Fibbo"
+          className="mt-4 border rounded mr-10 p-4 px-28 "          
+        />
+          <div className="hidden w-full md:block md:w-auto">
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+              <Link href="/">
+                <li>
+                  {/* <a className="block py-2 pl-3 pr-4 text-gray-700 text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</a> */}
+                  <a className="block py-2 pl-3 pr-4 text-purple-500 text-2xl font-bold text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Home</a>
+                </li> 
+              </Link>
+
+              <Link href="/create-item">
+                <li>
+                  {/* <a className="block py-2 pl-3 pr-4 text-gray-700 text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Create NFT</a> */}
+                  <a className="block py-2 pl-3 pr-4 text-purple-500 text-2xl font-bold text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Create NFT</a>
+                </li>
+              </Link>
+
+              <Link href="/my-nfts">
+                <li>
+                  {/* <a className="block py-2 pl-3 pr-4 text-gray-700 text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">My NFTs</a> */}
+                  <a className="block py-2 pl-3 pr-4 text-purple-500 text-2xl font-bold text-base border-b border-gray-100 hover:bg-gray-50 cursor-pointer md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">My NFTs</a>
+                </li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </nav>
       <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
+import web3 from 'web3'
 import axios from 'axios'
 import Web3Modal from "web3modal"
+import Image from 'next/image'
+import Link from 'next/link'
 
 import {
   nftaddress, nftmarketaddress
@@ -12,74 +15,71 @@ import Market from '../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 
 export default function Home() {
   const [nfts, setNfts] = useState([])
-  const [loadingState, setLoadingState] = useState('not-loaded')
+  const [loaded, setLoaded] = useState('not-loaded')
   useEffect(() => {
     loadNFTs()
   }, [])
+
   async function loadNFTs() {
-    /* create a generic provider and query for unsold market items */
-    const provider = new ethers.providers.JsonRpcProvider("https://rpc-mumbai.maticvigil.com")
+    const provider = new ethers.providers.JsonRpcProvider("https://rpc.testnet.fantom.network/")
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, Market.abi, provider)
     const data = await marketContract.fetchMarketItems()
-
-    /*
-    *  map over items returned from smart contract and format 
-    *  them as well as fetch their token metadata
-    */
+    
     const items = await Promise.all(data.map(async i => {
       const tokenUri = await tokenContract.tokenURI(i.tokenId)
       const meta = await axios.get(tokenUri)
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+      let price = web3.utils.fromWei(i.price.toString(), 'ether');
       let item = {
         price,
         tokenId: i.tokenId.toNumber(),
         seller: i.seller,
         owner: i.owner,
         image: meta.data.image,
-        name: meta.data.name,
-        description: meta.data.description,
       }
       return item
     }))
+    console.log('items: ', items)
     setNfts(items)
-    setLoadingState('loaded') 
+    setLoaded('loaded') 
   }
+
   async function buyNft(nft) {
-    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
-    const web3Modal = new Web3Modal()
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    });
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(nftmarketaddress, Market.abi, signer)
+    
+    const price = web3.utils.toWei(nft.price.toString(), 'ether');
 
-    /* user will be prompted to pay the asking proces to complete the transaction */
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')   
+    console.log('price: ', price);
+    
     const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
       value: price
     })
     await transaction.wait()
     loadNFTs()
   }
-  if (loadingState === 'loaded' && !nfts.length) return (<h1 className="px-20 py-10 text-3xl">No items in marketplace</h1>)
+  
+  if (loaded === 'loaded' && !nfts.length) return (<h1 className="p-20 text-4xl">No NFTs!</h1>)
   return (
     <div className="flex justify-center">
-      <div className="px-4" style={{ maxWidth: '1600px' }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+      <div style={{ width: 900 }}>
+        <div className="grid grid-cols-2 gap-4 pt-8">
           {
             nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} />
-                <div className="p-4">
-                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
-                  <div style={{ height: '70px', overflow: 'hidden' }}>
-                    <p className="text-gray-400">{nft.description}</p>
-                  </div>
-                </div>
-                <div className="p-4 bg-black">
-                  <p className="text-2xl mb-4 font-bold text-white">{nft.price} ETH</p>
-                  <button className="w-full bg-gradient-to-r from-pink-500 via-purple-500  text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
-                </div>
+              <div key={i} className="border p-4 shadow max-w-xs">
+                <Image src={nft.image} className="rounded" width="300px" height="250px" />
+                <p className="text-2xl my-4 font-bold">Price: {nft.price} FIB</p>
+                {/* <button className="bg-green-600 text-white py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy NFT</button> */}
+                <button className="bg-gradient-to-r from-blue-500 via-purple-500 text-white font-bold py-2 px-10 rounded" onClick={() => buyNft(nft)}>Comprar</button>
+                <Link href="/details-item">
+                <button className="bg-gradient-to-r from-blue-500 via-purple-500 text-white font-bold py-2 px-10 rounded">Detalle</button>                 
+                </Link>
               </div>
             ))
           }
